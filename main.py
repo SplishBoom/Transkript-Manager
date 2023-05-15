6e9a24faf497ebb69891f47959bedb637823b828
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-15 19:34:49 +0300

    add online unit test

--- main.py
+++ main.py
@@ -9,24 +9,34 @@ if __name__ == "__main__" :
 
     # ------------------------------------------------------------------------
 
-    test_file_paths = eval(os.getenv("test_file_paths"))
+    test_file_paths = eval(os.getenv("env_test_file_paths"))
     
-    parser = OfflineParser(path_to_file=test_file_paths[0], save_to_file=False)
+    username = str(os.getenv("env_username"))
+    password = str(os.getenv("env_password"))
+
+    parser = OnlineParser(username=username, password=password)
+    data0 = parser.get_transcript_data()
+
+    parser = OfflineParser(path_to_file=test_file_paths[0])
     data1 = parser.get_transcript_data()
 
-    parser = OfflineParser(path_to_file=test_file_paths[1], save_to_file=False)
+    parser = OfflineParser(path_to_file=test_file_paths[1])
     data2 = parser.get_transcript_data()
 
-    parser = OfflineParser(path_to_file=test_file_paths[2], save_to_file=False)
+    parser = OfflineParser(path_to_file=test_file_paths[2])
     data3 = parser.get_transcript_data()
 
     # ------------------------------------------------------------------------
 
-    connection_string = str(os.getenv("connection_string"))
-    database_name = str(os.getenv("database_name"))
+    connection_string = str(os.getenv("env_connection_string"))
+    database_name = str(os.getenv("env_database_name"))
 
     client = MongoClient(connection_string, database_name)
     
+    user_info_document, user_data_document = client.documentisize(data0)
+    client.user_info.push_init(user_info_document)
+    client.user_data.push_init(user_data_document)
+
     user_info_document, user_data_document = client.documentisize(data1)
     client.user_info.push_init(user_info_document)
     client.user_data.push_init(user_data_document)
@@ -40,3 +50,5 @@ if __name__ == "__main__" :
     client.user_data.push_init(user_data_document)
 
     client.close()
+
+    print("Done")
