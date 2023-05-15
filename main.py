7102ddfc4f03a88e05fadfb43b832d2dca59702a
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-15 23:47:15 +0300

    unit test moved

--- main.py
+++ main.py
@@ -1,54 +1,5 @@
-from Utilities import MongoClient, OfflineParser, OnlineParser
-from dotenv import load_dotenv
-import os
-
+from GUI import Application
 
 if __name__ == "__main__" :
 
-    load_dotenv()
-
-    # ------------------------------------------------------------------------
-
-    test_file_paths = eval(os.getenv("env_test_file_paths"))
-    
-    username = str(os.getenv("env_username"))
-    password = str(os.getenv("env_password"))
-
-    parser = OnlineParser(username=username, password=password)
-    data0 = parser.get_transcript_data()
-
-    parser = OfflineParser(path_to_file=test_file_paths[0])
-    data1 = parser.get_transcript_data()
-
-    parser = OfflineParser(path_to_file=test_file_paths[1])
-    data2 = parser.get_transcript_data()
-
-    parser = OfflineParser(path_to_file=test_file_paths[2])
-    data3 = parser.get_transcript_data()
-
-    # ------------------------------------------------------------------------
-
-    connection_string = str(os.getenv("env_connection_string"))
-    database_name = str(os.getenv("env_database_name"))
-
-    client = MongoClient(connection_string, database_name)
-    
-    user_info_document, user_data_document = client.documentisize(data0)
-    client.user_info.push_init(user_info_document)
-    client.user_data.push_init(user_data_document)
-
-    user_info_document, user_data_document = client.documentisize(data1)
-    client.user_info.push_init(user_info_document)
-    client.user_data.push_init(user_data_document)
-
-    user_info_document, user_data_document = client.documentisize(data2)
-    client.user_info.push_init(user_info_document)
-    client.user_data.push_init(user_data_document)
-
-    user_info_document, user_data_document = client.documentisize(data3)
-    client.user_info.push_init(user_info_document)
-    client.user_data.push_init(user_data_document)
-
-    client.close()
-
-    print("Done")
+    Application().mainloop()
