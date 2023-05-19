2194ea971fd32b07de09e7a178db900ef6f46a90
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-19 18:45:53 +0300

    update env with functions

--- Environment/__init__.py
+++ Environment/__init__.py
@@ -3,7 +3,11 @@ from Environment.constants import (
     PACKAGES_DC,
     SELENIUM_DC, # DONE
     ASSETS_DC,
-    UTILITIES_DC,
+    UTILITIES_DC, # DONE
     DATABASE_DC, # DONE
     GUI_DC
+)
+from Environment.constants import (
+    connect_pathes,
+    connect_urls
 )
