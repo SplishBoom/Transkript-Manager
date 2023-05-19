4ce0326ac2fe8c6c4949bf815fcc046479d2f854
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-19 19:17:17 +0300

    setup safe run entegration unit test

---
+++ UnitTests/unit_test_4.py
@@ -0,0 +1,12 @@
+"""
+UT : safe_start() and safe_end() test
+"""
+
+from Utilities import safe_start, safe_end
+
+if __name__ == '__main__':
+    safe_start()
+    """
+        DRIVER CODE HERE
+    """
+    safe_end()
