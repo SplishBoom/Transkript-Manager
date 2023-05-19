ec65a39a538f4eaa0bca1f548ec0baad640cbc9b
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-20 01:52:53 +0300

    add application setup

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,3 +1,4 @@
 from GUI.login import LoginFrame
+from GUI.application import ApplicationFrame
 
 from GUI.transcript_manager import TranscriptManager # Driver Code - import in last
---
+++ GUI/application.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+from tkinter import filedialog
+import os
+from Environment import ASSETS_DC, SELENIUM_DC
+
+class ApplicationFrame(ttk.Frame) :
+
+    def __init__(self, parent, root, DEBUG=False, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.root = root
+
+        self.__load_containers()
+
+        self.__load_user_info_label()
+
+    def __load_containers(self) :
+
+        self.container = ttk.Frame(self)
+        self.container.grid(row=0, column=0)
+
+        self.container.grid_rowconfigure((0,1,2), weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.user_info_label = ttk.Frame(self.container)
+        self.user_info_label.grid(row=0, column=0)
+
+    def __load_user_info_label(self) :
+        pass
