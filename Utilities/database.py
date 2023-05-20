ccf5d7ca2e375bd997851934d93c9d48a1a70ab9
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-20 18:33:54 +0300

    logical bug fix

--- Utilities/database.py
+++ Utilities/database.py
@@ -53,7 +53,7 @@ class MongoClient(pymongo.MongoClient):
             "filtering" : None,
             "sorting" : None,
             "modified_course_list" : None,
-            "document_name" : None,
+            "document_name" : "Transcript Manager",
             "subtracted_course_list" : None,
             "added_course_list" : None,
         }
