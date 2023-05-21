0570f0c94b0c9299fad1df1a247d48808925bee9
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-22 01:37:48 +0300

    fix init

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,4 +1,7 @@
-from GUI.login import LoginFrame
-from GUI.application import ApplicationFrame
 
+from GUI.prog_stat_analyzer import StatAnalyzer
+from GUI.prog_achievement_analyzer import AchievementAnalyzer
+from GUI.prog_grade_updater import GradeUpdater
+from GUI.application import ApplicationFrame
+from GUI.login import LoginFrame
 from GUI.transcript_manager import TranscriptManager # Driver Code - import in last
