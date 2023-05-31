46ac6a0778c17c91cab6e3647c02211acb3b70c2
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-05-31 20:49:15 +0300

    Remove development workscape for PDF Extraction

--- Utilities/PDF_DEV/DIRECT_EXECUTION/__init__.py
+++
@@ -1,9 +0,0 @@
-from DIRECT_EXECUTION.data_ops import (
-    sort_by,
-    filter_by,
-    add_course,
-    subtract_course,
-    update_course,
-    calculate_performance
-)
-from DIRECT_EXECUTION.translator import translate
